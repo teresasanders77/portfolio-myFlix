55760f4fbbcfb8529c5962f787cd542614b79c00
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2020-07-27 14:10:32 -0600

    tested file to make sure it was working correctly

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
-const http = require("http"),
+const http = require('http'),
   fs = require('fs');
-  url = require("url");
+  url = require('url');
 
 http
   .createServer((request, response) => {
-    let addr = request.url,
+    var addr = request.url,
       q = url.parse(addr, true);
       filePath = '';
 
     if (q.pathname.includes('documetation')) {
-      filePath = (__dirname + '/documentation.html');
+      filePath = __dirname + '/documentation.html';
     } else{
       filePath = 'index.html';
     }
 
-    fs.appendFile('log.text', 'URL:' + addr + '\nTimestamp:' + new Date() + '\n\n',(err) => {
+    fs.readFile(filePath, function(err, data) {
       if (err) {
-        console.log(err);
-      } else {
-        console.log('Added to log.');
+        throw err;
       }
-    });
 
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write(data)
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
     response.end();
   });
 
-}).listen(8080);
-console.log("My test server is running on Port 8080.");
+  fs.appendFile('log.text', 'URL:' + addr + '\nTimestamp:' + new Date() + '\n\n',
+  function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Added to log.');
+    }
+  }
+};
+})
+.listen(8080)
