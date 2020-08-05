90f1cc766ec14afff8d33d4e3586ebb1bb52bcb9
Teresa Loafman <teresasanders77@gmail.com>
2020-08-05 14:49:14 -0600

    heroku adjustment

--- index.js
+++ index.js
@@ -247,7 +247,7 @@ app.use(function (err, req, res, next) {
 });
 
 //listen for requests
-const port = process.env.PORT || 8080;
-app.listen(port, "0.0.0.0", () => {
-  console.log("Listening on Port " + port);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Listening on Port " + PORT);
 });
