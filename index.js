562335c312fa60502f81a6fb75012a51dfb7ba08
Teresa Loafman <teresasanders77@gmail.com>
2020-08-06 10:59:55 -0600

    Tidy up code

--- index.js
+++ index.js
@@ -1,39 +1,50 @@
-const express = require("express"),
-  bodyParser = require("body-parser"),
-  morgan = require("morgan"),
-  uuid = require("uuid"),
-  mongoose = require("mongoose");
-cors = require("cors");
-Models = require("./models.js");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Models = require("./models.js");
+const Movies = Models.Movie;
+const Users = Models.User;
+const { check, validationResult } = require("express-validator");
+uuid = require("uuid");
+const cors = require("cors");
 const passport = require("passport");
 require("./passport");
 
-const { check, validationResult } = require("express-validator");
+app.use(express.static("public"));
+app.use(morgan("common"));
+app.use(bodyParser.json());
 
-const app = express();
-const Movies = Models.Movie;
-const Users = Models.User;
+let allowedOrigins = [
+  "https://my-flix-77.herokuapp.com/",
+  "http://localhost:8080",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        //If a specific origin isn't found on the list of allowed origins
+        var message =
+          "The CORS policy for this application doesn't allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
+//Imports auth.js file
+let auth = require("./auth")(app);
 
 //mongoose.connect("mongodb://localhost:27017/myFlixDB", {
 mongoose.connect("process.env.CONNECTION_URI", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
 });
-
-//Using bodyParser
-app.use(bodyParser.json());
-
-//uses Morgan to log requests
-app.use(morgan("common"));
-
-//Imports auth.js file
-let auth = require("./auth")(app);
-
-let allowedOrigins = ["*"];
-
-//Using cors
-app.use(cors());
 
 //GET requests
 app.get("/", (req, res) => {
@@ -41,7 +52,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/documentation.html", (req, res) => {
-  res.sendFile("/public/documentation.html");
+  res.sendFile("documentation.html");
 });
 
 //Returns a list of ALL movies to the user
@@ -127,6 +138,7 @@ app.post(
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
+
     let hashedPassword = Users.hashPassword(req.body.Password);
     Users.findOne({ Username: req.body.Username }) //Search to see if a user with the requested username already exists
       .then((user) => {
@@ -156,32 +168,29 @@ app.post(
 );
 
 //Update a user's info, by username
-app.put(
-  "/users/:Username",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Users.findOneAndUpdate(
-      { Username: req.params.Username },
-      {
-        $set: {
-          Username: req.body.Username,
-          Password: req.body.Password,
-          Email: req.body.Email,
-          Birthday: req.body.Birthday,
-        },
+app.put("/users/:Username", (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
       },
-      { new: true }, //makes sure that the updated document is returned
-      (err, updatedUser) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send("Error: " + err);
-        } else {
-          res.json(updatedUser);
-        }
+    },
+    { new: true }, //makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      } else {
+        res.json(updatedUser);
       }
-    );
-  }
-);
+    }
+  );
+});
 
 //Add a movie to a user's list of favorites
 app.post(
@@ -249,16 +258,13 @@ app.delete(
   }
 );
 
-//serves "documentation.html" file from the public folder
-app.use(express.static("public"));
-
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
 
 //listen for requests
-const port = process.env.PORT || 8080;
-app.listen(port, "0.0.0.0", () => {
-  console.log("Listening on Port " + port);
+var port = process.env.PORT || 8080;
+app.listen(port, "0.0.0.0", function () {
+  //console.log("Listening on Port 8080");
 });
