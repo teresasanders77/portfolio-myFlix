1b56a8d03f20c06c50b6f482ac6d582672095f10
Teresa Loafman <teresasanders77@gmail.com>
2020-08-10 17:18:17 -0600

    removed cors

--- index.js
+++ index.js
@@ -18,21 +18,7 @@ app.use(bodyParser.json());
 
 var allowedOrigins = ['http://localhost:1234', '*'];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        //If a specific origin isn't found on the list of allowed origins
-        var message =
-          "The CORS policy for this application doesn't allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+app.use(cors());
 
 //Imports auth.js file
 let auth = require("./auth")(app);
