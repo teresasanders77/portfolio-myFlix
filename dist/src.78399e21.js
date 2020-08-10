d0d09de57a76c6ffa358f6ad8041e7947d637cce
Teresa Loafman <teresasanders77@gmail.com>
2020-08-10 17:13:36 -0600

    fix index.html

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -30473,7 +30473,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63776" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57927" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
