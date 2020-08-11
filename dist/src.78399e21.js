db1a89edffad299924a1d368ece9b9c215505fee
Teresa Loafman <teresasanders77@gmail.com>
2020-08-11 09:49:41 -0600

    update image file

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -30473,7 +30473,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57927" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61193" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
